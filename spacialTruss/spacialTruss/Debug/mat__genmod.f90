8d4535ca37e78834b5e54d120afb9879e60332a0
Xiaoxu Duan <xiaoxuduan@outlook.com>
2017-05-01 19:20:04 +0800

    modify something.
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    On branch mychange
    Changes to be committed:
    	modified:   spacialTruss/spacialTruss/Debug/BuildLog.htm
    	modified:   spacialTruss/spacialTruss/Debug/CLOSEF.obj
    	modified:   spacialTruss/spacialTruss/Debug/CONKB.obj
    	modified:   spacialTruss/spacialTruss/Debug/DATAIN.obj
    	modified:   spacialTruss/spacialTruss/Debug/DATAOUT.obj
    	modified:   spacialTruss/spacialTruss/Debug/DISPLS.obj
    	modified:   spacialTruss/spacialTruss/Debug/FKE.obj
    	modified:   spacialTruss/spacialTruss/Debug/FLMT.obj
    	modified:   spacialTruss/spacialTruss/Debug/FMAXA.obj
    	modified:   spacialTruss/spacialTruss/Debug/FT.obj
    	modified:   spacialTruss/spacialTruss/Debug/LDLT.obj
    	modified:   spacialTruss/spacialTruss/Debug/MAT.obj
    	modified:   spacialTruss/spacialTruss/Debug/MKFORCE.obj
    	modified:   spacialTruss/spacialTruss/Debug/OPENF.obj
    	modified:   spacialTruss/spacialTruss/Debug/RESOLVE.obj
    	modified:   spacialTruss/spacialTruss/Debug/TRUSS.obj
    	modified:   spacialTruss/spacialTruss/Debug/closef__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/closef__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/conkb__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/conkb__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/datain__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/datain__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/dataout__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/dataout__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/displs__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/displs__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/fke__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/fke__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/flmt__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/flmt__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/fmaxa__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/fmaxa__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/ft__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/ft__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/ldlt__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/ldlt__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/mat__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/mat__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/mkforce__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/mkforce__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/openf__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/openf__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/resolve__genmod.f90
    	modified:   spacialTruss/spacialTruss/Debug/resolve__genmod.mod
    	modified:   spacialTruss/spacialTruss/Debug/spacialTruss.exe
    	modified:   spacialTruss/spacialTruss/Debug/spacialTruss.pdb
    	modified:   spacialTruss/spacialTruss/Debug/vc140.pdb
    
    Changes not staged for commit:
    	deleted:    CLOSEF.f90
    	deleted:    CONKB.f90
    	deleted:    DATAIN.f90
    	deleted:    DATAOUT.f90
    	deleted:    DISPLS.f90
    	deleted:    Debug/makefile
    	deleted:    Debug/objects.mk
    	deleted:    Debug/source/subdir.mk
    	deleted:    Debug/sources.mk
    	deleted:    FKE.f90
    	deleted:    FLMT.f90
    	deleted:    FMAXA.f90
    	deleted:    FT.f90
    	deleted:    LDLT.f90
    	deleted:    MAT.f90
    	deleted:    MKFORCE.f90
    	deleted:    OPENF.f90
    	deleted:    RESOLVE.f90
    	deleted:    TRUSS.f90

--- spacialTruss/spacialTruss/Debug/mat__genmod.f90
+++ spacialTruss/spacialTruss/Debug/mat__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon May 01 18:31:28 2017
+        !COMPILER-GENERATED INTERFACE MODULE: Mon May 01 19:19:27 2017
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE MAT__genmod
